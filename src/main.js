1dd33a68e9653ecd8a42e806cf1a42992fc94899
BigJMoney <pipplepopple@gmail.com>
2022-02-13 01:49:46 -0500

    Simplified html and css structure, and implemented absolute sized module elements

--- src/main.js
+++ src/main.js
@@ -2,18 +2,30 @@ var is_adv_view = true
 
 function switch_main_view() {
     console.log('switching main view');
-    let adv_view = document.querySelector('#windows_center_adv_mode');
-    let arena_view = document.querySelector('#windows_center_arena_mode')
+    let focus_win = document.querySelector('#focus_win');
+    let activity_win = document.querySelector('#activity_win');
+    let prompt_win = document.querySelector('#prompt_win');
+
+    let arena_win = document.querySelector('#arena_win');
+    let aactivity_win = document.querySelector('#aactivity_win');
 
     if (is_adv_view) {
-        adv_view.style.display = 'none';
-        arena_view.style.display = 'grid';
-        is_adv_view = false
+        focus_win.style.display = 'none';
+        activity_win.style.display = 'none';
+        prompt_win.style.display = 'none';
+
+        arena_win.style.display = 'flex';
+        aactivity_win.style.display = 'flex';
+        is_adv_view = false;
     }
     else {
-        arena_view.style.display = 'none';
-        adv_view.style.display = 'grid';
-        is_adv_view = true
+        focus_win.style.display = 'flex';
+        activity_win.style.display = 'flex';
+        prompt_win.style.display = 'flex';
+
+        arena_win.style.display = 'none';
+        aactivity_win.style.display = 'none';
+        is_adv_view = true;
     }
 
 }
